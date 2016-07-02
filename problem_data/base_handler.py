289f741cb849d6eb6c209061ed4e2c2dfdcc0f8f
zrt <zrt@zrtdeMacBook-Pro.local>
2016-07-02 20:59:25 +0800

    merge

--- problem_data/base_handler.py
+++
@@ -1,5 +0,0 @@
-from tornado import httpserver,ioloop,web,gen,httpclient
-import urllib.parse
-from tools import *
-class BaseHandler(web.RequestHandler):
-    pass
--- problem_data/conf.py
+++
@@ -1,2 +0,0 @@
-DOWNLOAD_KEY = 'top-secret'
-SERVER_URL = 'http://localhost:5000' #用于展示的url，建议外网地址
